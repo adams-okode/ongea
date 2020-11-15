79e3a1904c72ec5a6157962fcf21ac0c89c9051a
adams-okode <adamsokode@gmail.com>
2020-11-15 18:10:15 +0300

    server added

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // the hello world program
-alert("Hello, World!");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
